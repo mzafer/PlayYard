bfeb59a8e82408f527bc0bae2c7d49b615c7cd34
zafer mohamed <m.zafer@tambolin.com>
2012-09-15 13:26:57 -0400

    Add more rest api files

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,36 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.bson.types.ObjectId
+import model.TestItem
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+ 
+    def getItem (id: String)= Action {
+        val oid = new ObjectId(id);
+        val testItem = TestItem.findOneById(oid)
+        Ok(TestItem.toCompactJson(testItem.get))
+    }
+    
+    def newItem = Action(parse.tolerantText) {request =>
+		 val jsonStr = request.body
+		 Logger.debug("Input Json :"+jsonStr)
+		 val testItem = TestItem.fromJSON(jsonStr)
+		 TestItem.insert(testItem)
+		 Logger.debug("Saved TestItem :"+TestItem.toCompactJson(testItem))
+		 Ok(TestItem.toCompactJson(testItem))
+	}
+    
+    def updateItem(id :String) = Action(parse.tolerantText) { request =>
+	     val jsonStr = request.body
+		 Logger.debug("Input Json for update TestItem:"+jsonStr)
+		 val testItem = TestItem.fromJSON(jsonStr)
+		 TestItem.save(testItem)
+		 Logger.debug("Updated TestItem :"+TestItem.toCompactJson(testItem))
+		 Ok(TestItem.toCompactJson(testItem))
+    }
 }
